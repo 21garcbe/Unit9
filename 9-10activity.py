74db0bfd410bd330a2451b35fdd037bce8a06d1d
Ben Garcia <21garcbe@gmail.com>
2026-03-11 16:11:14 -0400

    9-10 import activity

--- 9-10activity.py
+++ 9-10activity.py
@@ -1,1 +1,8 @@
 from restaurant import Restaurant
+
+texas_roadhouse = Restaurant('Texas Roadhouse')
+print(texas_roadhouse.restaurant_name)
+texas_roadhouse.set_number_served(10)
+print(texas_roadhouse.number_served)
+texas_roadhouse.increment_number_served(15)
+print(texas_roadhouse.number_served)
